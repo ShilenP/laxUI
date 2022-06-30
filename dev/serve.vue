6bc29e79fbd053c00a8b3a831009304ac29cd529
Shilen <sp1420@ic.ac.uk>
2022-06-30 16:57:20 +0100

    changed everything

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { BarkSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  BarkSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <bark-sample />
+  </div>
+</template>
